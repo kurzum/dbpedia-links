dec4e890445b35a6d937d3360290099e957004a8
Andre Valdestilhas <firmao@gmail.com>
2015-03-03 13:03:33 +0100

    Update CreateDB.sh

--- CreateDB.sh
+++ CreateDB.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sh ./nt_to_tsv.sh > file2.tsv
+while read line
+do
+    curl $line | bzcat | grep "\#sameAs" |  rapper -i ntriples -I - - file  2>/dev/null | cut -f1,3 -d '>' | sed 's/> </\t/g' | sed 's/> .&//g' | sed 's/^<//g' | sort -u
+done < links.txt > ld.tsv
 
 DBUSER=root
 DBPASSWORD=aux123
@@ -11,10 +14,6 @@ DBCONN="-h ${DBSERVER} -u ${DBUSER} --password=${DBPASSWORD}"
 
 echo "DROP DATABASE IF EXISTS ${DBNAME}" | mysql ${DBCONN}
 echo "CREATE DATABASE ${DBNAME}" | mysql ${DBCONN}
-echo "CREATE TABLE TEMPDUP (dbpedia_uri varchar(750) DEFAULT NULL, link_target varchar(750) DEFAULT NULL)" | mysql $DBCONN $DBNAME
+echo "CREATE TABLE RAW_SAMEAS (dbpedia_uri varchar(750) DEFAULT NULL, link_target varchar(750) DEFAULT NULL)" | mysql $DBCONN $DBNAME
 
-echo "LOAD DATA LOCAL INFILE 'file2.tsv' INTO TABLE TEMPDUP FIELDS TERMINATED BY '\t'" | mysql $DBCONN $DBNAME --local-infile=1
-
-echo "CREATE TABLE RAW_SAMEAS as (Select Distinct dbpedia_uri,link_target from TEMPDUP)" | mysql $DBCONN $DBNAME
-
-echo "DROP TABLE TEMPDUP" | mysql $DBCONN $DBNAME
+echo "LOAD DATA LOCAL INFILE 'ld.tsv' INTO TABLE RAW_SAMEAS FIELDS TERMINATED BY '\t'" | mysql $DBCONN $DBNAME --local-infile=1
